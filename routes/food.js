cee4d83cc93ad12d27acc5ba7e244935cf511f30
asif-khan-2k19 <mohammadasifkhan619@gmail.com>
2024-02-11 00:33:03 +0530

    Created Profile, Edit profile & Donate Pages

---
+++ routes/food.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var router = express.Router();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/food-connect");
+
+const foodSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  isAccepted: {
+    type: Boolean,
+    default: false
+  },
+  foodQuantity: {
+    type: Number,
+  },
+  foodItems: {
+    type: String,
+  },
+  pickupLocation: {
+    type: String,
+  },
+  pickupDate: {
+    type: String,
+  },
+  pickupTime: {
+    type: String,
+  },
+  donatedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+// foodSchema.plugin(plm);
+
+module.exports = mongoose.model("Food", foodSchema);
