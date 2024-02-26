241bf041493b51c8166b96e5f09d6140e5860bb0
asif-khan-2k19 <mohammadasifkhan619@gmail.com>
2024-02-26 20:09:59 +0530

    Fetching Donation details

--- routes/food.js
+++ routes/food.js
@@ -6,8 +6,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/food-connect");
 
 const foodSchema = new mongoose.Schema({
   date: {
-    type: Date,
-    default: Date.now(),
+    type: Date
   },
   isAccepted: {
     type: Boolean,
