82a3b0e34e89a374ca69b229837f55dec3adf0d1
asif-khan-2k19 <mohammadasifkhan619@gmail.com>
2024-03-19 18:10:55 +0530

    Added New Things

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,19 +1,3 @@
-// Dynamically getting time in donate page
-window.onload = function() {
-  var currentTimeInput = document.getElementById('currentTime');
-  var currentDateInput = document.getElementById('currentDate');
-  
-  var now = new Date();
-  var hours = now.getHours().toString().padStart(2, '0');
-  var minutes = now.getMinutes().toString().padStart(2, '0');
-  var currentTime = hours + ':' + minutes;
-
-  currentTimeInput.value = currentTime;
-  
-  var year = now.getFullYear().toString().padStart(4, '0');
-  var month = (now.getMonth() + 1).toString().padStart(2, '0');
-  var day = now.getDate().toString().padStart(2, '0');
-  var currentDate = year + '-' + month + '-' + day;
-  
-  currentDateInput.value = currentDate;
-};
+function goBack(){
+  window.history.back();
+}
