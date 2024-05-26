a7be96df1382e7fd7efa1ee47a51b5a2b5c81d9d
asif-khan-2k19 <mohammadasifkhan619@gmail.com>
2024-05-26 15:39:13 +0530

    Connecting to atlas

--- routes/food.js
+++ routes/food.js
@@ -3,8 +3,8 @@ var router = express.Router();
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-// mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect("mongodb://127.0.0.1:27017/food-connect")
+mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect("mongodb://127.0.0.1:27017/food-connect")
 
 const foodSchema = new mongoose.Schema({
   date: {
--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
 require("dotenv").config();
 
-// mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect("mongodb://127.0.0.1:27017/food-connect")
+mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect("mongodb://127.0.0.1:27017/food-connect")
 
 const userSchema = new mongoose.Schema({
   name: {
